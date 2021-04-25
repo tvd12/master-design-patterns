ce840e6198bf846bddef61b2a485037bde9bfeb5
Dung Ta Van <itprono3@gmail.com>
2021-04-25 19:39:04 +0700

    update observer: add SumCalculator

---
+++ src/main/java/com/tvd12/designparttern/observer/SumCalculator.java
@@ -0,0 +1,33 @@
+package com.tvd12.designparttern.observer;
+
+import java.util.function.Consumer;
+
+public class SumCalculator {
+
+	public static int sumWithReturn(int a, int b) {
+		return a + b;
+	}
+	
+	public static void sumWithCallback(int a, int b, Consumer<Integer> callback) {
+		int sum = a + b;
+		callback.accept(sum);
+	}
+	
+	public static void sumAsync(int a, int b, Consumer<Integer> callback) {
+		int sum = a + b;
+		Thread newThread = new Thread(() -> callback.accept(sum));
+		newThread.start();
+	}
+	
+	public static void main(String[] args) throws Exception {
+		System.out.println("sumWithReturn: " + sumWithReturn(1, 2));
+		sumWithCallback(1, 2, it -> {
+			System.out.println("sumWithReturn: " + it);
+		});
+		sumAsync(1, 2, it -> {
+			System.out.println("sumAsync: " + it);
+		});
+		Thread.sleep(1000);
+	}
+	
+}
