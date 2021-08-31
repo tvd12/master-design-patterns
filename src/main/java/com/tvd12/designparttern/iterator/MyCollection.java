6976efc7ac69148f2853c1debe6cfd2efbccb3f0
Dung Ta Van <itprono3@gmail.com>
2021-08-31 13:13:34 +0700

    add MyCollection

---
+++ src/main/java/com/tvd12/designparttern/iterator/MyCollection.java
@@ -0,0 +1,45 @@
+package com.tvd12.designparttern.iterator;
+
+import java.util.Iterator;
+
+public class MyCollection<T> implements Iterable<T> {
+
+	private int size = 0;
+	private Object[] items = new Object[100];
+	
+	public void add(T item) {
+		this.items[size ++] = item;
+	}
+	
+	@Override
+	public Iterator<T> iterator() {
+		return new MyIterator();
+	}
+	
+	public static void main(String[] args) {
+		MyCollection<String> collection = new MyCollection<>();
+		collection.add("a");
+		collection.add("b");
+		collection.add("c");
+		for (String item : collection) {
+			System.out.print(item + ", ");
+		}
+	}
+	
+	class MyIterator implements Iterator<T> {
+
+		private int index;
+		
+		@Override
+		public boolean hasNext() {
+			return index < size;
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public T next() {
+			return (T)items[index ++];
+		}
+	}
+
+}
