a7140c2db4dbaafd1bcea241c595f75659a1e1d4
Dung Ta Van <itprono3@gmail.com>
2025-07-04 12:21:48 +0700

    update composite design pattern

--- chapter-021-composite/src/main/java/com/tvd12/master_design_patterns/request/AddAuthorRequest.java
+++ chapter-021-composite/src/main/java/com/tvd12/master_design_patterns/request/AddAuthorRequest.java
@@ -5,4 +5,5 @@ import lombok.Data;
 @Data
 public class AddAuthorRequest {
     private String authorName;
+    private int age;
 }
--- chapter-021-composite/src/main/java/com/tvd12/master_design_patterns/validator/AuthorValidator.java
+++ chapter-021-composite/src/main/java/com/tvd12/master_design_patterns/validator/AuthorValidator.java
@@ -3,20 +3,55 @@ package com.tvd12.master_design_patterns.validator;
 import com.tvd12.ezyhttp.core.exception.HttpBadRequestException;
 import com.tvd12.master_design_patterns.request.AddAuthorRequest;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static com.tvd12.ezyfox.io.EzyStrings.isBlank;
 
 public class AuthorValidator {
 
+    private final List<Validator> validators;
+
+    public AuthorValidator() {
+        this.validators = Arrays.asList(
+            new AuthorNameValidator(),
+            new AuthorAgeValidator()
+        );
+    }
+
     public void validate(AddAuthorRequest request) {
         final Map<String, String> errors = new HashMap<>();
-        if (isBlank(request.getAuthorName())) {
-            errors.put("authorName", "required");
+        for (Validator validator : validators) {
+            validator.validate(request, errors);
         }
         if (!errors.isEmpty()) {
             throw new HttpBadRequestException(errors);
         }
+    }
+
+    private static class AuthorNameValidator implements Validator {
+
+        @Override
+        public void validate(AddAuthorRequest request, Map<String, String> errors) {
+            if (isBlank(request.getAuthorName())) {
+                errors.put("authorName", "required");
+            }
+        }
+    }
+
+    private static class AuthorAgeValidator implements Validator {
+
+        @Override
+        public void validate(AddAuthorRequest request, Map<String, String> errors) {
+            if (request.getAge() < 0) {
+                errors.put("authorName", "invalid");
+            }
+        }
+    }
+
+    private interface Validator {
+        void validate(AddAuthorRequest request, Map<String, String> errors);
     }
 }
