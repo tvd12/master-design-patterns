f9574ad3c38f78c4a81831dab560b305f43747b2
tvd12 <ta.van.dung@youngmonkeys.org>
2026-06-12 21:07:30 +0700

    update

--- chapter-019-adapter/src/main/java/com/tvd12/master_design_patterns/operation/NullOperation.java
+++
@@ -1,6 +0,0 @@
-package com.tvd12.master_design_patterns.operation;
-
-public interface NullOperation<T> {
-
-    T operate();
-}
--- chapter-024-proxy/src/main/java/com/tvd12/master_design_patterns/factory/EntityFactory.java
+++ chapter-024-proxy/src/main/java/com/tvd12/master_design_patterns/factory/EntityFactory.java
@@ -6,5 +6,9 @@ public interface EntityFactory {
 
     <E> E newEntity(Class<E> entityType, String name);
 
+    default <E> E newNullEntity(Class<E> entityType) {
+        return newEntity(entityType, "");
+    }
+
     <E, B extends Builder<E>> B newEntityBuilder(Class<B> builderType);
 }
